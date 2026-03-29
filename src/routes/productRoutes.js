2efd4513acf6aca73b56f5ae211d40e7b2fc7183
Matheus <matheusviana3038@gmail.com>
2026-03-29 18:39:36 -0300

    Comentei a rota de produtos

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -8,17 +8,22 @@ const { Product, ProductOption, Category, ProductCategory, ProductImage } = requ
 
 router.get('/product/search', async(req, res) => {
     try{
+        //Pega os query params da request
         let { limit = 30, page = 1, fields, match, category_ids, "price-range" : priceRange, ...rest } = req.query;
 
+        //Passa o valor de do limit e da page para Inteiro
         limit = parseInt(limit); 
         page = parseInt(page);
 
+        //Verifica se o limit e o page sao numeros, se nao, retorna Bad Request
         if(isNaN(limit) || isNaN(page)){
             return res.status(400).json({message: "Parametros invalidos"});
         }
 
+        //Cria o where principal
         let where = {};
 
+        //Verifica se o match tem algum valor,  se sim, atribui ele dentro do where
         if(match) {
             where[Op.or] = [
                 {name: { [Op.iLike]: `%${match}%`}},
@@ -26,6 +31,7 @@ router.get('/product/search', async(req, res) => {
             ];
         }
 
+        //Verifica se o price range tem valor, se sim, separa o valor dele em Min e Max, verifica se sao numeros e atribui no where.price
         if(priceRange){
             const[min, max] = priceRange.split('-').map(Number);
             if(!isNaN(min) && !isNaN(max)) {
@@ -33,11 +39,13 @@ router.get('/product/search', async(req, res) => {
             }
         }
 
+        //Atribui o where no objeto query options e adiciono distinct: true para evitar valores repetidos
         let queryOptions = {
             where, 
             distinct: true
         };
 
+        //Verifica se category_ids tem algum valor, se sim, adciono seu valor tratado em ids, saindo de "3, 4, 2" para [3, 4, 2], e crio a atributo include
         if(category_ids){
             const ids = category_ids.split(',').map(Number);
 
@@ -51,6 +59,7 @@ router.get('/product/search', async(req, res) => {
             })
         }
 
+        //Crio a variavel optionFilter para uma pesquisa com as opcoes dos produtos
         let optionFilters = [];
 
         Object.keys(rest).forEach(key => {
@@ -67,6 +76,7 @@ router.get('/product/search', async(req, res) => {
             }
         });
 
+        //Verifica e o limit tem valor igual a -1, se sim, atribui ao queryOptions.limit, e cria o offset
         if(limit !== -1){
             queryOptions.limit = limit;
             queryOptions.offset = (page - 1) * limit;
@@ -87,12 +97,15 @@ router.get('/product/search', async(req, res) => {
             });
         }
 
+        //Verifica se fields tem algum valor, se sim, atribui esse valor a queryOptions.attributes
         if(fields){
             queryOptions.attributes = fields.split(',');
         }
 
+        //Pega o resultado e o total da query na tabela de produtos no banco de dados passando a query option que foi montada anteriormente
         const { rows, count } = await Product.findAndCountAll(queryOptions);
 
+        //Retorna os dados com codigo de sucesso
         res.status(200).json({
             data: rows,
             total: count,
@@ -107,36 +120,45 @@ router.get('/product/search', async(req, res) => {
 
 router.get('/product/:id', async(req, res) => {
     try{
+        //Pega o ID do produto na request
         const prodId = req.params.id;
 
+        //Pesquisa o produto na tabela do banco de dados pelo ID e atribui o resultado para a variavel result
         const result = await Product.findByPk(prodId);
 
+        //Pega os campos de relevancia do result
         const { id, enabled, name, slug, stock, description, price, price_with_discount } = result;
 
+        //Pesquisa os IDs das categorias relacionadas com com o ID do produto
         const ids = await ProductCategory.findAll({
             where: {product_id: prodId},
             attributes: ['category_id']
         });
 
+        //Pega as options que estao relacionadas com o ID do produto
         const options = await ProductOption.findAll({
             where: {
                 product_id: prodId
             }
         })
 
+        //Pega as imagens que estao relacionadas com o ID do produto
         const images = await ProductImage.findAll({
             where: { product_id: id },
             attributes: ['id', 'path']
         })
 
+        //Cria a variavel category_ids
         let category_ids = [];
 
+        //Atribui cada posicao do array ids no novo array category_ids
         if(ids.length){
             ids.forEach(id => {
                 category_ids.push(id.category_id);
             })
         }
 
+        //Retorna os dados
         return res.status(200).json({
             id: id,
             enabled: enabled, 
@@ -158,12 +180,15 @@ router.get('/product/:id', async(req, res) => {
 
 router.post('/product', authMiddleware, async(req, res) => {
     try{
+        //Pega os dados da request
         const { enabled, name, slug, stock, description, price, price_with_discount, category_ids, images, options } = req.body;
 
+        //Verifica se estao vazios, se sim, retorna Bad Request
         if(!enabled || !name || !slug || !stock || !description || !price || !category_ids || !images || !options){
             return res.status(400).json({message: "Preencha todos os campos"});
         }
 
+        //Cria o produto com os dados da request e atribui a resposta a variavel productReturn
         const productReturn = await Product.create({
             enabled: enabled,
             name: name,
@@ -174,6 +199,7 @@ router.post('/product', authMiddleware, async(req, res) => {
             price_with_discount: price_with_discount
         });
 
+        //Cria o relacionamento do produto com as categorias selecionadas na request
         for (catId of category_ids) {
             await ProductCategory.create({
                 product_id: productReturn.id,
@@ -181,6 +207,7 @@ router.post('/product', authMiddleware, async(req, res) => {
             })
         }
 
+        //Verifica se o array options tem algum valor, se sim, percorre esse array criando as options que se relacionam com o produto criado
         if(options){
             for (opt of options) {
                 await ProductOption.create({
@@ -194,6 +221,7 @@ router.post('/product', authMiddleware, async(req, res) => {
             }
         }
 
+        //Verifica se o array images tem algum valor, se sim, cria o relacionamento das imagens com o produto no banco de dados
         if(images){
             imagesNumber = images.length;
             imagesArray = new Array(imagesNumber).fill("uploads/imagem-teste.png");
@@ -206,6 +234,7 @@ router.post('/product', authMiddleware, async(req, res) => {
             }
         }
 
+        //Retorna sucesso
         return res.status(201).json({message: "Produto criado com sucesso", data: {id: productReturn.id}});
     } catch(err) {
         console.log(err);
@@ -215,9 +244,13 @@ router.post('/product', authMiddleware, async(req, res) => {
 
 router.put('/product/:id', authMiddleware, async(req, res) => {
     try{
+        //Pega o ID do produto no parametros da request
         const prodId = req.params.id;
+
+        //Pega os valores do corpo da requisicao
         const { enabled, name, slug, stock, description, price, price_with_discount, category_ids, images, options } = req.body;
 
+        //Atualiza o produto de acordo com ID fornecido
         const result = await Product.update({
             enabled: enabled,
             name: name,
@@ -232,6 +265,12 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         }
     );
 
+    //Verifico se o update deu certo
+    if(!result){
+        return res.status(500).json({message: "Erro ao atualizar o produto"})
+    }
+
+    //Pega o ID das categorias atualmente relacionadas com o ID do produto e atribui em ids
     const ids = await ProductCategory.findAll({
         where: {
             product_id: prodId
@@ -239,20 +278,30 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         attributes: ['category_id']
     });
 
+    //Crio a variavel actual_cat_ids para atribuir os IDs das categorias atualmente relacionadas com esse produto
     let actual_cat_ids = ids.length > 0 ? ids.map(id => id.category_id) : [];
+    
+    //Crio a variavel newCats para atribuir os IDs das categorias que vieram na request, se o actual_cat_ids estiver vazio, atribuo o category_ids da request como valor
     let newCats = actual_cat_ids.length === 0 ? category_ids : [];
 
+    //Verifico de o actual_cat_ids esta vazio
     if(actual_cat_ids.length){
+        //Percorro o category_ids
         for (catId of category_ids) {
+            //Verifico se o valor de catId esta presente no actual_cat_ids
             if(actual_cat_ids.includes(parseInt(catId))){
+                //Se sim, retiro esse valor de dentro do array de categorias atuais
                 actual_cat_ids = actual_cat_ids.filter(id => id !== catId);
             } else{
+                //Se nao estiver, adiciono no array de novas categorias
                 newCats.push(catId);
             }
         }
     }
 
+    //Verifico se sobrou algum valor no array de categorias atuais, os valores que sobrarem nesse array sao relacionamentos que devem ser apagados
     if(actual_cat_ids.length){
+        //Se sim, apago da tabela de relacionamento
         await ProductCategory.destroy({
             where: { 
                 category_id:{ [Op.in]: actual_cat_ids },
@@ -261,8 +310,11 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         })
     }
 
+    //Verifico se o array de novas categorias tem algum valor, os valores desse array sao novos relacionamentos de categoria com produto que devem ser criados
     if(newCats.length){
+        //Percorro esse array
         for (catId of newCats){
+            //Pra cada ID de categoria eu crio um relacionamento com o produto
             await ProductCategory.create({
                 product_id: prodId,
                 category_id: catId
@@ -270,13 +322,17 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         }
     }
 
+    //Verifico se o array de imagens tem algum valor
     if(images){
+        //Se sim, percorro ele
         for (img of images){
+            //Verifico se o atributo deleted existe, se sim, apago essa imagem
             if(img.deleted){
                 await ProductImage.destroy({
                     where: { id: img.id }
                 })
             }
+            //Verifico se o atributo ID existe e verifico se o atributo deleted nao existe, porque imagens com o deleted tambem tem ID, se sim, atualizo a imagem
             else if(img.id && !img.deleted){
                 await ProductImage.update({
                     enabled: img.enabled || true,
@@ -285,6 +341,7 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
                     where: { id: img.id }
                 }) 
             } 
+            //Se nao tiver nem deleted nem ID, crio o novo relacionamento de produto imagem no banco de dados
             else{
                 await ProductImage.create({
                     enabled: img.enabled || true, 
@@ -295,16 +352,18 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         }
     }
 
+    //Verifico se o array options tem algum valor
     if(options.length > 0){
+        //Se sim percorro ele
         for (opt of options) {
+            ////Verifico se o atributo deleted existe, se sim, apago essa imagem
             if(opt.deleted){
                 await ProductOption.destroy({
                     where: { id: opt.id }
                 })
             } 
-
+            //Verifico se o atributo ID existe e verifico se o atributo deleted nao existe, porque options com o deleted tambem tem ID, se sim, atualizo a option
             if(opt.id && !opt.deleted){
-                console.log(opt)
                 await ProductOption.update({
                     title: opt.title || "",
                     shape: opt.shape || "square",
@@ -317,6 +376,7 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
                 }
                 )
             } 
+            //Se nao tiver nem deleted nem ID, crio o novo relacionamento de produto com option no banco de dados
             else{
                 await ProductOption.create({
                     product_id: prodId,
@@ -329,9 +389,8 @@ router.put('/product/:id', authMiddleware, async(req, res) => {
         }
     }
 
-    if(result.length){
-        return res.status(204).json();
-    }
+    //Retorno sucesso
+    return res.status(204).json();
 
     } catch(err) {
         console.log(err);
