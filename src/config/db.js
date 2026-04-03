c9b32240553c5c921a5076bd4b983dfaad26607e
Matheus <matheusviana3038@gmail.com>
2026-04-03 20:27:08 -0300

    ajustes

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // importante pro Neon
+        rejectUnauthorized: false,
       },
     },
   }
